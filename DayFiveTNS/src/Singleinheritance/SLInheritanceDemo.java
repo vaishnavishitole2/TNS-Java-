2e4f2e02a379f15bd98f7eb57cdd9ecd74adfa54
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2024-12-28 17:45:03 +0530

    Initial commit

---
+++ DayFiveTNS/src/Singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package Singleinheritance;
+
+public class SLInheritanceDemo {
+
+public static void main(String[] args) {
+		
+		Student st = new Student("Vaishnavi", "808080808080","Pune", "9561353211",10, "G.H.Raisoni");
+		System.out.println(st);
+
+	}
+}
