cb8fe8751f79824ca117f54a89fd243ea23ed276
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2024-12-28 11:21:16 +0530

    Initial commit

---
+++ DayThreeTNS/src/DayThree/TNS/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package DayThree.TNS;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(100);
+		obj.setName("Vaishhnavi");
+		obj.setAge(21);
+		
+		System.out.println(obj);
+
+	}
+
+}
---
+++ DayThreeTNS/src/DayThree/TNS/OopsConceptDemo.java
@@ -0,0 +1,33 @@
+package DayThree.TNS;
+	public class OopsConceptDemo {
+		
+		private int serialNum;
+		private String name;
+		private int age;
+		
+		
+		public int getSerialNum() {
+			return serialNum;
+		}
+		public void setSerialNum(int serialNum) {
+			this.serialNum = serialNum;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		@Override
+		public String toString() {
+			return "OopsConceptDemo [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+		}
+
+}
