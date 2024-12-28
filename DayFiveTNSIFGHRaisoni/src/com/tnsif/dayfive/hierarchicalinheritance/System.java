267567b7f0838ab65bb2daab4517ca9e6319d7fa
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2024-12-28 19:57:33 +0530

    Initial commit

---
+++ DayFiveTNSIFGHRaisoni/src/com/tnsif/dayfive/hierarchicalinheritance/System.java
@@ -0,0 +1,12 @@
+package com.tnsif.dayfive.hierarchicalinheritance;
+
+public class System {
+
+	public static Object out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
